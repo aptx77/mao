3b83a8d2eab60600f8cfbbb5697e433b53567385
aptx77 <107793517+aptx77@users.noreply.github.com>
2023-04-08 20:39:32 +0800

    Update 量子影视.js

--- 0824/js/量子影视.js
+++ 0824/js/量子影视.js
@@ -1,1 +1,8 @@
-/sg.json
+var rule= {
+模板:'mxone5',
+title:'量子影视',
+host:'http://www.lzizy9.com',
+url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+searchUrl:'/index.php/vod/search.html?wd=**',
+cate_exclude:'网址'
+}
