3c4733972c2bb1f203259b46ab9b81d4ee0439e8
aptx77 <107793517+aptx77@users.noreply.github.com>
2023-04-08 21:07:09 +0800

    Update vip影院.js

--- 0824/js/vip影院.js
+++ 0824/js/vip影院.js
@@ -1,1 +1,6 @@
-/sg.json
+var rule = Object.assign(muban.vfed,{
+title:'VIP影院',
+host:'http://360yy.cn',
+url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+});
