0cba9ab1e4d199ccd265d4a90232f8790a2dcaa7
aptx77 <107793517+aptx77@users.noreply.github.com>
2023-04-08 20:38:02 +0800

    Update 蓝光影院.js

--- 0824/js/蓝光影院.js
+++ 0824/js/蓝光影院.js
@@ -1,1 +1,7 @@
-/sg.json
+var rule = Object.assign(muban.mxpro,{
+title:'蓝光影院',
+host:'https://lgyy.cc',
+url:'/vodshow/fyclass--------fypage---.html',
+searchUrl:'/vodsearch/**-------------.html',
+class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
+});
